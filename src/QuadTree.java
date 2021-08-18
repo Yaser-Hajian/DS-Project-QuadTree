8bdf1a658d37c0f116ef79515249aa0971243e27
Yaser <mohammadyaser1379@gmail.com>
2021-08-18 10:02:19 +0430

    insert method in QuadTree class completed

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -23,7 +23,52 @@ class Node{
     }
 }
 public class QuadTree {
+    public Node root;
 
+    public QuadTree(){
+        root=null;
+    }
+
+    public void insert(Point point){
+        Node newNode = new Node(point);
+        if (root == null){
+            root=newNode;
+            return;
+        }
+        Node current = root;
+        Node parent = null;
+        while (true){
+            parent = current;
+            if (point.x >= current.point.x && point.y > current.point.y){
+                current = current.top_right;
+                if (current == null){
+                    parent.top_right = newNode;
+                    return;
+                }
+            }
+            else if (point.x > current.point.x && point.y <= current.point.y){
+                current = current.down_right;
+                if (current == null){
+                    parent.down_right = newNode;
+                    return;
+                }
+            }
+            else if (point.x < current.point.x && point.y >= current.point.y){
+                current = current.top_left;
+                if (current == null){
+                    parent.top_left = newNode;
+                    return;
+                }
+            }
+            else if (point.x <= current.point.x && point.y < current.point.y){
+                current = current.down_left;
+                if (current == null){
+                    parent.down_left = newNode;
+                    return;
+                }
+            }
+        }
+    }
 }
 class Main{
     public static void main(String[] args) {
