1137c45788ff984f6e4aea4a1734792e34547bd8
Yaser <mohammadyaser1379@gmail.com>
2021-08-18 10:00:55 +0430

    Node class completed

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -8,7 +8,19 @@ class Point{
     }
 }
 class Node{
+    Point point;
+    Node top_right;
+    Node top_left;
+    Node down_right;
+    Node down_left;
 
+    public Node(Point point) {
+        this.point = point;
+        this.top_left = null;
+        this.top_right = null;
+        this.down_left = null;
+        this.down_right = null;
+    }
 }
 public class QuadTree {
 
